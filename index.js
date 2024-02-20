90f262e4b83af0df1ddd6f5e4cdd14d6f2cbd073
Mahadi Hasan <mahadimhs787@gmail.com>
2024-02-20 18:09:03 +0600

    third commi

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -9,8 +11,8 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://bcsDB:szjIuBI3dU3RE6HD@cluster0.wpfolqw.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wpfolqw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +28,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const applicationCollection = client.db('bcsDb').collection('applications')
+
+    app.get('/applicants', async(req, res) => {
+        const result = await applicationCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.post('/applications', async(req, res) => {
+        const query = req.body;
+        const result = await applicationCollection.insertOne(query);
+        res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -37,7 +53,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Mahadi')
 })
 
 app.listen(port, () => {
